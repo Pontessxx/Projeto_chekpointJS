5be6fe1c5b462ea75f46c4543ef3dfa8383d3e7d
Pontes <hpo.pontes@gmail.com>
2023-09-06 12:18:57 -0300

    Navbar finalizado

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,13 +3,14 @@ import { Link, Route, Routes, BrowserRouter } from 'react-router-dom'
 import Products from '../../pages/Products'
 import Home from '../../pages/Home'
 import Sobre from '../../pages/Sobre'
-
+import './Nav.css'
 const Navbar = () => {
   return (
     <>
         <BrowserRouter>
-            <nav>
-                <ul>
+            <nav className='navbar'>
+                <div className='logo'>Front<span>end</span></div>
+                <ul className='menu'>
                     <li>
                         <Link to="/">Home</Link>
                     </li>
